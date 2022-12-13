e5c98f504f958b29a66c18c4fad2f522010d6e93
Jags <jon.kmjt@gmail.com>
2022-12-12 21:19:19 -0500

    commit with all functionalties

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,33 +4,28 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var specialChar = ["!","@","#","$","%","^","&","*","(",")"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
-// Get references to the #generate element
+// variable initialized to button on webpage
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// this function will write the password to the webpage using query selector
 function writePassword() {
   var password = generatePassword(password);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 function generatePassword(){
-/* CHECK FOR LENGTH TO DO:::
-    if (document.querySelector("#lengthPW").value == ""){
-        var passwordText = document.querySelector("#password");
-        passwordText.value = "Please Select a password length";              
-    }
-    */
+    //inititalizing all variables needed from html IDs
     var passwordLength = document.getElementById("lengthPW").value;
     var specialCharYN = document.getElementById("specialCharYN").value;
     var upperCaseYN = document.getElementById("upperCaseYN").value;
     var lowerCaseYN = document.getElementById("lowerCaseYN").value;
     var numericYN = document.getElementById("numericYN").value;
+    //tempPassord to save current password and add to it with each iteration
     var tempPassword = "";
 
     while(tempPassword.length < passwordLength){
-
+        //each iteration based on the length of password selected will add a character to a string "tempPassword" of each type that was selected on the webpage
         if(specialCharYN == "true"){
             tempPassword = specialChar[Math.floor(Math.random() * specialChar.length)].concat(tempPassword);
 
@@ -64,10 +59,10 @@ function generatePassword(){
         }
 
     }
-
+    //returns the password to the writePassword function
     return tempPassword;
 }
 
 
-// Add event listener to generate button
+// event listener that calls writePassword function on click
 generateBtn.addEventListener("click", writePassword);
